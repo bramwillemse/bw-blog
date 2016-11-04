880077478eeb137a486579ee7cd8a66b498a69b6
Bram Willemse <contact@bramwillemse.nl>
2016-11-04 21:38:59 +0100

    Switch to display inline-block instead of float left for grid

--- web/app/themes/bramster/header.php
+++ web/app/themes/bramster/header.php
@@ -78,10 +78,4 @@
 
 <body <?php body_class(); ?>>
 
-	<header id="top" class="l-header toolbar">
-        <a href="<?php bloginfo('url'); ?>" class="logo">
-    		<span class="logo__name"><?php bloginfo('name'); ?></span>
-        	<span class="logo__description"><?php bloginfo('description'); ?></span>
-        </a>
-    	<?php get_template_part('inc/nav-main') ?>
-	</header>
+    <?php get_template_part('inc/toolbar'); ?>
