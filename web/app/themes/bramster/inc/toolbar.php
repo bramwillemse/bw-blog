c00490f6f694a2b739178782bbd8755fc718de0e
Bram Willemse <contact@bramwillemse.nl>
2016-11-04 21:39:51 +0100

    Separate toolbar as module

---
+++ web/app/themes/bramster/inc/toolbar.php
@@ -0,0 +1,10 @@
+<header id="top" class="toolbar">
+    
+    <a href="<?php bloginfo('url'); ?>" class="logo">
+        <span class="logo__name"><?php bloginfo('name'); ?></span>
+        <span class="logo__description"><?php bloginfo('description'); ?></span>
+    </a>
+
+    <?php get_template_part('inc/nav-main') ?>
+
+</header>
